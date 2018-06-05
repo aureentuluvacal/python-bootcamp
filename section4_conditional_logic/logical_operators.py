6d1e6278b8ebed614bda7f1b52db19cb1dddaf32
Caryssa Perez <caryssaperez@gmail.com>
2018-06-04 20:57:01 -0400

    Section 4

---
+++ section4_conditional_logic/logical_operators.py
@@ -0,0 +1,8 @@
+print(6 >= 9)
+print(6 == 9)
+print(6 != 9)
+
+age = 50
+print(age > 69 and age < 100)
+
+print(not age == 75)
---
+++ section4_conditional_logic/simple_logic.py
@@ -0,0 +1,11 @@
+print("What's your favorite pizza chain?")
+favorite = input()
+
+if favorite == "Domino's":
+  print("Excellent, best choice.")
+elif favorite == "Pizza Hut":
+  print("Solid choice.")
+elif favorite == "Papa John's":
+  print("What the fuck is wrong with you?")
+else:
+  print("Ok")
