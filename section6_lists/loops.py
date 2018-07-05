98e3452cb186fa749890313fee73273804870f06
Caryssa Perez <caryssaperez@gmail.com>
2018-07-05 15:44:13 -0400

    While loop too

--- section6_lists/loops.py
+++ section6_lists/loops.py
@@ -7,3 +7,10 @@ numbers = [1, 2, 3, 4, 5]
 
 for number in numbers:
   print(number * 2)
+
+# While loop example
+index = 0
+
+while index < len(colors):
+  print(f"Color {index}: {colors[index]}")
+  index += 1
