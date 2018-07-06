1748971818ff8b0e01d4e3eba6010cc9e7950c09
Caryssa Perez <caryssaperez@gmail.com>
2018-07-06 13:23:56 -0400

    Sets

--- section8_tuples_and_sets/sets.py
+++ section8_tuples_and_sets/sets.py
@@ -9,3 +9,17 @@ for num in s:
 cities = { "Chicago", "Chicago", "Indianapolis", "Columbus", "St. Louis", "Columbus"}
 
 print(set(cities))
+
+cities.add("New York")
+cities.remove("St. Louis")
+cities.discard("Oslo")
+
+more_cities = cities.copy()
+more_cities.clear()
+
+more_cities = { "Chicago", "Terre Haute", "Miami" }
+
+print(cities | more_cities)
+print(cities & more_cities)
+
+# Find a union
