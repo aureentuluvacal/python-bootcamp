a002f7f5295d82309e8355881ba86b432bf30fc0
Caryssa Perez <caryssaperez@gmail.com>
2018-07-06 14:47:03 -0400

    Start functions

---
+++ section9_functions/birthday.py
@@ -0,0 +1,7 @@
+def sing_happy_birthday():
+  print("Happy birthday to you\n")
+  print("Happy birthday to you\n")
+  print("Happy birthday dear someone\n")
+  print("Happy birthday to you\n")
+
+sing_happy_birthday()
