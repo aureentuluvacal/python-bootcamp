d8facd35762e8ac1faf6286c082a6fcea465d993
Caryssa Perez <caryssaperez@gmail.com>
2018-07-06 15:02:35 -0400

    Handling params

---
+++ section9_functions/params.py
@@ -0,0 +1,16 @@
+def square(num):
+  return num**2
+
+print(square(4))
+print(square(8))
+
+def multiply(first, second):
+  return first * second
+
+print(multiply(11, 17))
+
+def exponent(num, power = 2):
+  return num ** power
+
+print(exponent(4,3))
+print(exponent(5))
