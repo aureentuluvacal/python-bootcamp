6c3e8d8c7499f81c5dc25a6c23fc3547e59f623a
Caryssa Perez <caryssaperez@gmail.com>
2018-07-06 11:50:01 -0400

    Finish other dict methods

--- section7_dictionaries/methods.py
+++ section7_dictionaries/methods.py
@@ -1,7 +1,8 @@
 teacher = {
   "name": "Colt Steele",
   "age": "who knows",
-  "gender": "M"
+  "gender": "M",
+  "likesCats": True
 }
 
 clone = teacher.copy()
@@ -12,6 +13,16 @@ print(dictionary)
 
 name = teacher.get("name")
 print(name)
+
+teacher.pop("gender")
+print(teacher)
+
+teacher.popitem()
+print(teacher)
+
+second_teacher = {}
+second_teacher.update(teacher)
+print(second_teacher)
 
 teacher.clear()
 print(teacher)
