f089a11f949aaea2ba2586b430b97c8e02faa23b
Caryssa Perez <caryssaperez@gmail.com>
2018-07-15 17:21:33 -0400

    Make art

--- section12_modules/art.py
+++ section12_modules/art.py
@@ -1,16 +1,17 @@
-import pyfiglet
+from pyfiglet import figlet_format
 from termcolor import colored
 
-message = print(input("What message do you want to print? "))
-color = print(input("And in what color? "))
+def print_art(message, color): 
+  # Check for actual colors.
+  valid_colors = ["grey", "red", "green", "yellow", "blue", "magenta", "cyan", "white"]
 
-# Check for actual colors.
-valid_colors = ["grey", "red", "green", "yellow", "blue", "magenta", "cyan", "white"]
+  if color not in valid_colors:
+    color = "magenta"
 
-if color not in valid_colors:
-  color = "magenta"
+  art = figlet_format(message)
+  colored_art = colored(art, color=color)
+  print(colored_art)
 
-art = pyfiglet.figlet_format(message)
-colored_art = colored(art, color=color)
-
-print(colored_art)
+message = input("What message do you want to print? ")
+color = input("And in what color? ")
+print_art(message, color)
